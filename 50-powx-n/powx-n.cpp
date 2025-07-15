85d95c57d2b646531fbc4dd0fcd39b33dd858276
Vishesh Rathod <147051172+visheshnayak1@users.noreply.github.com>
2025-07-15 17:32:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (70.30%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;  // handle edge case of INT_MIN
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        
+        double result = 1;
+        while (N > 0) {
+            if (N % 2 == 1) {  // if odd
+                result *= x;
+            }
+            x *= x;
+            N /= 2;
+        }
+        return result;
+    }
+};
